be182c3456971688f7f318895d1d98e808b7e622
Andy Bangs <andrew.yuta@gmail.com>
2015-10-27 22:41:13 -0700

    NavigatorIOS -> Navigator

--- index.ios.js
+++ index.ios.js
@@ -2,21 +2,67 @@
 
 var React = require('react-native');
 var {
-  AlertIOS,
-  AsyncStorage,
   AppRegistry,
-  NavigatorIOS,
+  AsyncStorage,
+  Navigator,
+  PixelRatio,
   StyleSheet,
+  Text,
+  TouchableOpacity,
 } = React;
 
 var MainScreen = require('./App/Components/MainScreen');
 var SettingsScreen = require('./App/Components/SettingsScreen');
+
 var RATIO = 'ratio';
+
+var NavigationBarRouteMapper = {
+  LeftButton(route, navigator, index, navState) {
+    if (index === 0) {
+      return null;
+    }
+
+    var previousRoute = navState.routeStack[index - 1];
+    return (
+      <TouchableOpacity
+        onPress={() => navigator.pop()}
+        style={styles.navBarLeftButton}>
+        <Text style={[styles.navBarText, styles.navBarButtonText]}>
+          Back
+        </Text>
+      </TouchableOpacity>
+    );
+  },
+
+  RightButton(route, navigator, index, navState) {
+    if (index === 1) {
+      return null;
+    }
+    return (
+      <TouchableOpacity
+        onPress={() => navigator.push({title: 'Ratio', index: 1})}
+        style={styles.navBarRightButton}>
+        <Text style={[styles.navBarText, styles.navBarButtonText]}>
+          Settings
+        </Text>
+      </TouchableOpacity>
+    );
+  },
+
+  Title(route, navigator, index, navState) {
+    return (
+      <Text style={[styles.navBarText, styles.navBarTitleText]}>
+        {route.title}
+      </Text>
+    );
+  }
+};
 
 class CoffeeAndWater extends React.Component{
   constructor(props) {
     super(props);
 
+    this.renderScene = this.renderScene.bind(this);
     this._onRatioChange = this._onRatioChange.bind(this);
 
     this.state = {
@@ -40,54 +86,40 @@ class CoffeeAndWater extends React.Component{
     .done();
   }
 
-  render() {
-    var initialRoute = {
-      component: MainScreen,
-      title: 'Coffe + Water',
-      rightButtonTitle: 'Settings',
-      onRightButtonPress: this._onSettingsPress.bind(this),
-      passProps: {
-        ratio: this.state.ratio,
-        coffee: this.state.coffee,
-        water: this.state.water,
-      },
-    };
-
-    return (
-      <NavigatorIOS
-        ref='nav'
-        style={styles.container}
-        initialRoute={initialRoute}
-      />
-    );
+  renderScene(route, nav) {
+    if (route.index === 0) {
+      return (
+        <MainScreen
+          ratio={this.state.ratio}
+          coffee={this.state.coffee}
+          water={this.state.water}
+        />
+      );
+    } else {
+      return (
+        <SettingsScreen
+          ratio={this.state.ratio}
+          parentCallback={this._onRatioChange}
+        />
+      );
+    }
   }
 
-  _onSettingsPress() {
-    var ratio = this.state.ratio;
-
-    this.refs.nav.push({
-      title: 'Ratio',
-      component: SettingsScreen,
-      leftButtonTitle: 'Back',
-      onLeftButtonPress: () => this.refs.nav.pop(),
-      rightButtonTitle: 'Save',
-      onRightButtonPress: () => {
-        AlertIOS.alert(
-          'TODO:',
-          'Persist Settings',
-          [
-            {
-              text: 'OK',
-              onPress: () => console.log('Tapped OK'),
-            },
-          ]
-        );
-      },
-      passProps: {
-        ratio: ratio,
-        parentCallback: this._onRatioChange,
-      },
-    });
+  render() {
+    return (
+      <Navigator
+        debugOverlay={false}
+        style={styles.appContainer}
+        initialRoute={{title: 'Coffee + Water', index: 0}}
+        renderScene={this.renderScene}
+        navigationBar={
+          <Navigator.NavigationBar
+            routeMapper={NavigationBarRouteMapper}
+            style={styles.navBar}
+          />
+        }
+      />
+    );
   }
 
   _onRatioChange(value) {
@@ -97,12 +129,29 @@ class CoffeeAndWater extends React.Component{
       water: Math.floor(this.props.coffee / value),
     });
   }
-
 };
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  navBar: {
+    backgroundColor: 'white',
+  },
+  navBarText: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  navBarTitleText: {
+    color: '#373e4d',
+    fontWeight: '500',
+    marginVertical: 9,
+  },
+  navBarLeftButton: {
+    paddingLeft: 10,
+  },
+  navBarRightButton: {
+    paddingRight: 10,
+  },
+  navBarButtonText: {
+    color: '#5890ff',
   },
 });
 
